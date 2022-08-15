0ee670ab5828762f8d3e3c11f9113fb9ef136df3
EduardoLangner <eduardodossantos752@gmail.com>
2022-08-15 11:50:53 -0300

    new repository

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+
+
+// app.get('/' , (req, res) => {
+//     res.send('Hello World!')
+// })
+
+
+module.exports = app
