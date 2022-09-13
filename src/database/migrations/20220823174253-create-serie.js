64be9c004737a268e61bc6dfc681bf099f9cc169
Eduardo dos Santos Langner <69880659+EduardoLangner@users.noreply.github.com>
2022-09-12 21:18:40 -0300

    Adicionado imagem nas tables

--- src/database/migrations/20220823174253-create-serie.js
+++ src/database/migrations/20220823174253-create-serie.js
@@ -43,6 +43,11 @@ module.exports = {
         allowNull: false,
       },
 
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
